d372b05e66df285147486304c36141d0ff071d9d
Ade Bateman <bateman@acm.org>
2017-02-27 22:02:32 -0800

    Add user info and status tests.

--- test/test.js
+++ test/test.js
@@ -22,4 +22,28 @@ describe('Testing BabyConnect',() => {
             assert(!result,"login should resolve with false for failure");
         });
     });
+
+    it('should return user info',() => {
+        let bc = new BabyConnect(username,password);
+        return bc.login()
+        .then(result => {
+            assert(result,"login should resolve with true for success");
+            return bc.getUserInfo();
+        }).then(result => {
+            assert(result.myKids.length>0,"myKids collection should not be empty");
+        });
+    });
+
+    it('should return kid status',() => {
+        let bc = new BabyConnect(username,password);
+        return bc.login()
+        .then(result => {
+            assert(result,"login should resolve with true for success");
+            return bc.getUserInfo();
+        }).then(result => {
+            return bc.getStatus('170226',result.myKids[0].Id);
+        }).then(result => {
+            assert(result.list.length>0,"list collection should not be empty");
+        });
+    });
 });
