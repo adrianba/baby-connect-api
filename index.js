6a398ccba1917c798bf22cc6c8194242377edd96
Ade Bateman <bateman@acm.org>
2017-04-02 14:12:32 -0700

    Reformat with prettier.

--- index.js
+++ index.js
@@ -1,40 +1,52 @@
-const BabyConnect = require('./baby-connect');
+const BabyConnect = require("./baby-connect");
 
 const username = process.env.BABYCONNECTUSER;
 const password = process.env.BABYCONNECTPWD;
 
-var bc = new BabyConnect(username,password);
+var bc = new BabyConnect(username, password);
 var kid;
-bc.login().then(result => {
-  if(!result) throw 'Login error';
-  return bc.getUserInfo();
-}).then(result => {
-  console.log(result);
-  return bc.lookupKid('George');
-}).then(k => {
-  kid = k;
-  console.log(kid);
-  return bc.getStatus({kid});
-}).then(result => {
-  console.log(result);
-  return bc.getSleepStatus({kid});
-}).then(result => {
-  console.log(result);
-  return bc.getBottleStatus({kid});
-}).then(result => {
-  console.log(result);
-  return bc.getNursingStatus({kid});
-}).then(result => {
-  console.log(result);
-  return bc.getPumpingStatus({kid});
-}).then(result => {
-  console.log(result);
-  return bc.getDiaperStatus({kid});
-}).then(result => {
-  console.log(result);
-  return bc.getChildList();
-}).then(result => {
-  console.log(result);
-}).catch(error => {
-  console.error(error);
-});
+bc
+  .login()
+  .then(result => {
+    if (!result) throw "Login error";
+    return bc.getUserInfo();
+  })
+  .then(result => {
+    console.log(result);
+    return bc.lookupKid("George");
+  })
+  .then(k => {
+    kid = k;
+    console.log(kid);
+    return bc.getStatus({ kid });
+  })
+  .then(result => {
+    console.log(result);
+    return bc.getSleepStatus({ kid });
+  })
+  .then(result => {
+    console.log(result);
+    return bc.getBottleStatus({ kid });
+  })
+  .then(result => {
+    console.log(result);
+    return bc.getNursingStatus({ kid });
+  })
+  .then(result => {
+    console.log(result);
+    return bc.getPumpingStatus({ kid });
+  })
+  .then(result => {
+    console.log(result);
+    return bc.getDiaperStatus({ kid });
+  })
+  .then(result => {
+    console.log(result);
+    return bc.getChildList();
+  })
+  .then(result => {
+    console.log(result);
+  })
+  .catch(error => {
+    console.error(error);
+  });
--- test/test.js
+++ test/test.js
@@ -1,54 +1,57 @@
 "use strict";
 
 const assert = require("assert");
-const BabyConnect = require('../baby-connect.js');
+const BabyConnect = require("../baby-connect.js");
 
 const username = process.env.BABYCONNECTUSER;
 const password = process.env.BABYCONNECTPWD;
 
-describe('Testing BabyConnect',() => {
-    it('new should be optional',() => {
-        let bc = BabyConnect(username,password);
-        assert(bc instanceof BabyConnect);
-    });
+describe("Testing BabyConnect", () => {
+  it("new should be optional", () => {
+    let bc = BabyConnect(username, password);
+    assert(bc instanceof BabyConnect);
+  });
 
-    it('should login with correct credientials',() => {
-        let bc = BabyConnect(username,password);
-        return bc.login()
-        .then(result => {
-            assert(result,"login should resolve with true for success");
-        });
+  it("should login with correct credientials", () => {
+    let bc = BabyConnect(username, password);
+    return bc.login().then(result => {
+      assert(result, "login should resolve with true for success");
     });
+  });
 
-    it('should fail login with incorrect credientials',() => {
-        let bc = BabyConnect("demo@demo.com","demo");
-        return bc.login()
-        .then(result => {
-            assert(!result,"login should resolve with false for failure");
-        });
+  it("should fail login with incorrect credientials", () => {
+    let bc = BabyConnect("demo@demo.com", "demo");
+    return bc.login().then(result => {
+      assert(!result, "login should resolve with false for failure");
     });
+  });
 
-    it('should return user info',() => {
-        let bc = BabyConnect(username,password);
-        return bc.login()
-        .then(result => {
-            assert(result,"login should resolve with true for success");
-            return bc.getUserInfo();
-        }).then(result => {
-            assert(result.myKids.length>0,"myKids collection should not be empty");
-        });
-    });
+  it("should return user info", () => {
+    let bc = BabyConnect(username, password);
+    return bc
+      .login()
+      .then(result => {
+        assert(result, "login should resolve with true for success");
+        return bc.getUserInfo();
+      })
+      .then(result => {
+        assert(result.myKids.length > 0, "myKids collection should not be empty");
+      });
+  });
 
-    it('should return kid status',() => {
-        let bc = BabyConnect(username,password);
-        return bc.login()
-        .then(result => {
-            assert(result,"login should resolve with true for success");
-            return bc.getUserInfo();
-        }).then(result => {
-            return bc.getStatus({kid:result.myKids[0].Id});
-        }).then(result => {
-            assert(result.list.length>0,"list collection should not be empty");
-        });
-    });
+  it("should return kid status", () => {
+    let bc = BabyConnect(username, password);
+    return bc
+      .login()
+      .then(result => {
+        assert(result, "login should resolve with true for success");
+        return bc.getUserInfo();
+      })
+      .then(result => {
+        return bc.getStatus({ kid: result.myKids[0].Id });
+      })
+      .then(result => {
+        assert(result.list.length > 0, "list collection should not be empty");
+      });
+  });
 });
